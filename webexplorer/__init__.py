29b093ead1bbd9b6bccd898a82a19adb9a8b5287
p.felczak@gmail.com <p.felczak@gmail.com>
2019-06-26 21:59:15 +0200

    project structure

---
+++ webexplorer/__init__.py
@@ -0,0 +1,20 @@
+from core import *
+
+if __name__ == "__main__":    
+    appContext = AppContext('config.cfg')
+    appContext.GetConfiguration()
+    print('URL = ' + appContext.data['Url'])
+    for x in range(1):
+        timePoint = datetime.datetime.now()
+        print('------------------------------------')
+        print('Loop | Start | ' + str(x))                
+        program = WebExplorer(appContext, True)
+        program.Go()
+        timePoint = datetime.datetime.now() - timePoint
+        ite = program.links.__len__()
+        for x in range(ite):
+
+            pass
+        print('Loop | End | ' + str(timePoint))
+        print(*program.links, sep = '\n')
+    pass
